68e2f99b8e6d82e9634fa5b4ef189e76c55e50dc
John Lloyd Sereno <166397885+toshfry@users.noreply.github.com>
2024-04-23 00:16:47 +0800

    Add files via upload

---
+++ IPT REVIEW/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.reviews-slider', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+});
