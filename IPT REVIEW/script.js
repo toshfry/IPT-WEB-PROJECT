158237d93afd16a921857d15db8b67c5c6de3325
John Lloyd Sereno <166397885+toshfry@users.noreply.github.com>
2024-04-23 01:40:25 +0800

    Update script.js

--- IPT REVIEW/script.js
+++ IPT REVIEW/script.js
@@ -1,13 +1,22 @@
-var swiper = new Swiper('.reviews-slider', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.querySelectorAll('.swiper-slide').forEach(function (slide) {
+
+        slide.querySelectorAll('.custom-star-rating').forEach(function (starRating) {
+            starRating.addEventListener('click', function (event) {
+                const clickedStar = event.target;
+                if (clickedStar.classList.contains('fa-star')) {
+                    const rating = parseInt(clickedStar.getAttribute('data-rating'));
+                    const stars = starRating.querySelectorAll('.fa-star');
+                    stars.forEach((star, index) => {
+                        if (index < rating) {
+                            star.classList.add('checked');
+                        } else {
+                            star.classList.remove('checked');
+                        }
+                    });
+                }
+            });
+        });
+    });
 });
